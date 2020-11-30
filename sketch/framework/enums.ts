b3a453c1175e60128a25ea86598ca2e1b7ff6a63
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-11-29 23:34:04 -0300

    Update enums.ts

--- sketch/framework/enums.ts
+++ sketch/framework/enums.ts
@@ -1,15 +1,18 @@
-export type ColorName = 'blue' | 'green' | 'red' | 'yellow' | 'orange' | 'pink' | 'transparent';
+import { ISpriteSheet } from "./animation/interfaces";
+
+export type AvailableColor = 'blue' | 'green' | 'red' | 'yellow' | 'orange' | 'pink' | 'transparent';
+export type ResourceContentType = p5.Image | p5.Font | p5.SoundFile | ISpriteSheet | string;
 export enum ShapeType {
     rect,
     circle
 }
 
 export enum ResourceType {
-    image,
-    spriteSheet,
-    sound,
-    font,
-    text
+    image = "Image",
+    spriteSheet = "SpriteSheet",
+    sound = "Sound",
+    font = "Font",
+    text = "Text"
 }
 
 
