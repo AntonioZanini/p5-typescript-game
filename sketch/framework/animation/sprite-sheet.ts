5d1c093e9fb66ea934a5ccb3dd5045032d27078a
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-11-29 23:33:57 -0300

    Update sprite-sheet.ts

--- sketch/framework/animation/sprite-sheet.ts
+++ sketch/framework/animation/sprite-sheet.ts
@@ -1,13 +1,15 @@
 import { ISpriteSheet, ICoord, IShape, ISpriteFrame } from './interfaces'
-import { ShapeType } from '../enums'
+import { AvailableColor, ShapeType } from '../enums'
+import { IRecolor, IResource, IResourceController, ISpriteImportResource } from '../controller/interfaces';
 
-class SpriteSheet implements ISpriteSheet {
+export class SpriteSheet implements ISpriteSheet {
     id: string;
     spriteSheet: p5.Image;
     spriteWidth: number;
     spriteHeight: number;
     spriteNumber: number;
     spritesByRow: number;
+    recolors?: Array<IRecolor>;
     private frameArray: Array<ICoord>;
 
     constructor(
@@ -17,13 +19,30 @@ class SpriteSheet implements ISpriteSheet {
         spriteHeight: number,
         spriteNumber: number,
         spritesByRow: number,
+        recolors?: Array<IRecolor>
     ) {
         this.id = id;
         this.spriteSheet = spriteSheet;
         this.spriteWidth = spriteWidth;
         this.spriteHeight = spriteHeight;
         this.spriteNumber = spriteNumber;
-        this.spritesByRow = spritesByRow
+        this.spritesByRow = spritesByRow;
+        this.recolors = recolors;
+    }
+
+    static constructFromImportedResource(
+        importedResource : ISpriteImportResource, 
+        spriteSheetImage: p5.Image) : ISpriteSheet {
+
+        return new SpriteSheet(
+            importedResource.id,
+            spriteSheetImage,
+            importedResource.spriteWidth,
+            importedResource.spriteHeight,
+            importedResource.spriteNumber,
+            importedResource.spritePerRow,
+            importedResource.recolors
+        );
     }
 
     drawSprite(spriteFrame: ISpriteFrame, location: IShape): void {
@@ -57,6 +76,37 @@ class SpriteSheet implements ISpriteSheet {
                 collisionShape.diameter
             );
         }
+    }
+
+    generateColoredSpriteSheets(resourceController: IResourceController): Array<IResource> {
+        const colors = this.recolors as Array<IRecolor>;
+        const recolorList: Array<IResource> = [];
+        colors.forEach(recolor => {
+            const tintedImage = this.tintSpriteSheet(resourceController, recolor.colorName);
+            const itemID = `${this.id}_${recolor.sufix}`;
+            recolorList.push({
+                id: itemID,
+                content: new SpriteSheet(
+                            `${this.id}_${recolor.sufix}`,
+                            tintedImage,
+                            this.spriteWidth,
+                            this.spriteHeight,
+                            this.spriteNumber,
+                            this.spritesByRow
+                )});
+        });
+        return recolorList;
+    }
+
+    private tintSpriteSheet(resourceController: IResourceController, color: AvailableColor) : p5.Image {
+        const graphics = createGraphics(this.spriteSheet.width, this.spriteSheet.height);
+
+        graphics.imageMode(CORNER);
+        graphics.tint(resourceController.getColor(color));
+        graphics.image(this.spriteSheet, 0, 0);
+        const tintedImage = graphics.get();
+        graphics.noTint();
+        return tintedImage;
     }
 }
 
