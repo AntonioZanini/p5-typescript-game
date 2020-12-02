a0a602d6d1d94f6cd967d0188cdd54e2043d4bd3
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-12-01 23:41:03 -0300

    Update enums.ts

--- sketch/enums.ts
+++ sketch/enums.ts
@@ -1,1 +1,7 @@
-export type AvailableScenes = 'intro' | 'main_menu' | 'credits' | 'stage1' | 'stage2' | 'boss' | 'game_over';
+export type AvailableScenes = 'intro' | 'main_title' | 'credits' | 'stage1' | 'stage2' | 'boss' | 'game_over';
+
+export enum SceneState {
+    intro,
+    playing,
+    ending
+}
