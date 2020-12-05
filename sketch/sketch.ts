ce77a1971c931bf88382d0860bb5d24099cfa282
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-12-05 10:10:24 -0300

    Update sketch.ts

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -3,6 +3,7 @@ import SceneController from './framework/controller/scene-controller';
 import GameController from './framework/controller/game-controller';
 import KeyEventController from './framework/controller/key-event-controller';
 import ResourceController from './framework/controller/resource-controller'
+import MainTitle from './game/scenes/main-title';
 
 const resourceController = new ResourceController();
 const keyEventController = new KeyEventController();
@@ -10,12 +11,14 @@ const gameController = new GameController();
 const sceneController = new SceneController<AvailableScenes>(gameController, resourceController, keyEventController);
 
 function preload() {
-    resourceController.importFromJSON('');
+    resourceController.importFromJSON('assets/json/resources.json');
 }
   
 function setup() {
     resourceController.load();
-    //sceneController.addScenes();
+    sceneController.addScenes(
+        new MainTitle()
+    );
 }
   
 function draw() {
