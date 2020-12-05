68b4819331d828536e32bf3d5b38241fc56c5b82
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-12-05 10:10:21 -0300

    Update main-title.ts

--- sketch/game/scenes/main-title.ts
+++ sketch/game/scenes/main-title.ts
@@ -7,6 +7,7 @@ import SceneBase from "./scene-base"
 
 export default class MainTitle extends SceneBase<AvailableScenes> 
     implements IScene<AvailableScenes>, IKeyEventListener {
+    private nextScene : AvailableScenes;
     private state : SceneState = SceneState.intro;
     private currentDirection: number = 0;
     private speed: number = 5;
@@ -19,42 +20,50 @@ export default class MainTitle extends SceneBase<AvailableScenes>
     handleKeyEvent(keyCode: number): void {
         switch (keyCode) {
             case LEFT_ARROW:
-                
+                this.currentDirection = -1;
                 break;
-            default:
+            case RIGHT_ARROW:
+                this.currentDirection = 1;
                 break;
         }
     }
 
     initialize(): void {
-        this.backgroundImage = new BackgroundImage("main-background");
-        this.backgroundSound = this.controller.resourceController.getResource<p5.SoundFile>(ResourceType.sound, "main-theme");
+        this.backgroundImage = new BackgroundImage("bgi-main-title");
+        this.backgroundSound = this.controller.resourceController.getResource<p5.SoundFile>(ResourceType.sound, "bgm-main-title");
         this.backgroundSound.loop();
     }
 
     private executeCommon(): void {
         this.backgroundImage.update(getSumModifier(this.currentDirection * this.speed));
+        this.currentDirection = 0;
         this.backgroundImage.draw();
     }
 
     private executeIntro(): void {
-        
+        let endIntro : boolean = false;
+
+        if (endIntro) { this.state = SceneState.playing }
     }
 
     private executeMain(): void {
+        let endMain : boolean = false;
 
+        if (endMain) { this.state = SceneState.ending }
     }
 
     private executeEnding(): void {
-        
+        let endEnding : boolean = false;
+
+        if (endEnding) { this.controller.changeScene(this.nextScene); }
     }
 
     playScene(): void {
         this.executeCommon();
 
         if (this.state == SceneState.intro) { this.executeIntro(); }
-        if (this.state == SceneState.playing) { this.executeMain(); }
-        if (this.state == SceneState.ending) { this.executeEnding(); }
+        else if (this.state == SceneState.playing) { this.executeMain(); }
+        else if (this.state == SceneState.ending) { this.executeEnding(); }
     }
 
 
